1b82c1e2cc46892b7eecd21c0e68e07b3e01ea1f
MSA-BD <140373331+MSA-BD@users.noreply.github.com>
2023-08-13 23:55:43 +0600

    reduce function

--- reduceFunction.js
+++ reduceFunction.js
@@ -4,3 +4,4 @@ ei method diye array er sokol complex kaj kora zay
 reduce() method duita parametre recieve kore, 1. callback function, 2. default value
 */ 
 newArr=[1,2,3,4,5,6,7,8]
+const sum=newArr.reduce(()=>{},0)
