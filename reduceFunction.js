0a9c06f896c836daf9d338f6774cf8eadff39e3c
MSA-BD <140373331+MSA-BD@users.noreply.github.com>
2023-08-13 23:54:30 +0600

    reduce callback

--- reduceFunction.js
+++ reduceFunction.js
@@ -1,5 +1,6 @@
 /* Al things about advance JS topic reduce function
 array.reduce() method main array ke change kore na
 ei method diye array er sokol complex kaj kora zay
+reduce() method duita parametre recieve kore, 1. callback function, 2. default value
 */ 
 newArr=[1,2,3,4,5,6,7,8]
