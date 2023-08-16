4856fc50fb54ebde283182d11421ee64421ffee2
MSA-BD <140373331+MSA-BD@users.noreply.github.com>
2023-08-16 23:59:13 +0600

    arrayreturn value

--- iterativeArray.js
+++ iterativeArray.js
@@ -1,5 +1,7 @@
 /*Iterative array: An iterative arrays is an array that every slot/item accessed one by one  by looping.
 Kichu method main array ke poriborton kore na, main array er refference nijer moddhye  copy kore kaj kore ebong notun arekti reffrence array return kore, ei reffrence main array er refference ke point/refer kore.
 Array er primitive type valuer kkhetre value gulu notun array-te copy hoy.
-Mutating array method ke non-mutate e convert korar jonno existing array ke kuno non-muatate method diye access kore nite hoy: arr.slice().copyWithin(1,2,3)
+Mutating array method ke non-mutate e convert korar jonno existing array ke kuno non-muatate method othoba spread-syntex diye access kore nite hoy: arr.slice().copyWithin(1,2,3)
+const value=[...arr].copywithin(1,2,3)
 */ 
+//These method some, every, find, findIndex, findLast, and findLastIndex do not always invoked the callback function, they stop iteration as soon as they return value is detremined.
