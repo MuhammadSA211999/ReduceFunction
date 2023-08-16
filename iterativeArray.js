8ebc20382436402d9c5cb6aecbb30227b7f61eca
MSA-BD <140373331+MSA-BD@users.noreply.github.com>
2023-08-16 23:16:14 +0600

    array reffrence

--- iterativeArray.js
+++ iterativeArray.js
@@ -1,2 +1,3 @@
 /*Iterative array: An iterative arrays is an array that every slot/item accessed one by one  by looping.
+Kichu method main array ke poriborton kore na, main array er refference nijer moddhye  copy kore kaj kore ebong notun arekti reffrence array return kore, ei reffrence main array er refference ke point/refer kore.
 */ 
