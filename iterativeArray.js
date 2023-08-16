3947fb5f65cb17571af6099e4a8152568a68da08
MSA-BD <140373331+MSA-BD@users.noreply.github.com>
2023-08-16 23:35:17 +0600

    mutate method theke non-mutate

--- iterativeArray.js
+++ iterativeArray.js
@@ -1,3 +1,5 @@
 /*Iterative array: An iterative arrays is an array that every slot/item accessed one by one  by looping.
 Kichu method main array ke poriborton kore na, main array er refference nijer moddhye  copy kore kaj kore ebong notun arekti reffrence array return kore, ei reffrence main array er refference ke point/refer kore.
+Array er primitive type valuer kkhetre value gulu notun array-te copy hoy.
+Mutating array method ke non-mutate e convert korar jonno existing array ke kuno non-muatate method diye access kore nite hoy: arr.slice().copyWithin(1,2,3)
 */ 
