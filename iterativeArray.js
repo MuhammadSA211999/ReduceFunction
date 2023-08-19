8ff9ca499daf9f76224ebf9076d9c8d4f14039a2
MSA-BD <140373331+MSA-BD@users.noreply.github.com>
2023-08-19 12:22:41 +0600

    cheking existance of an item of an array

--- iterativeArray.js
+++ iterativeArray.js
@@ -4,4 +4,12 @@ Array er primitive type valuer kkhetre value gulu notun array-te copy hoy.
 Mutating array method ke non-mutate e convert korar jonno existing array ke kuno non-muatate method othoba spread-syntex diye access kore nite hoy: arr.slice().copyWithin(1,2,3)
 const value=[...arr].copywithin(1,2,3)
 //These method some, every, find, findIndex, findLast, and findLastIndex do not always invoked the callback function, they stop iteration as soon as they return value is detremined.
-*/ 
+*/
+
+//kun ekti item arrayte ache kina ta check korar jonno: includes()/indexOf() method er vitore item er nam argument hisabe pass korte hoy; includes()=>boolean and indexOf()=> int/-1 return kore
+const fruits = ['ban', 'apl', 'jack']
+const result = fruits.indexOf('gg')
+// console.log('ban'); // array te item takle index, na takle -1 return kore
+const blResult = fruits.includes('apl')
+console.log(blResult); //array te item takle true, na takle false return kore
+
