aa68372472429963121a8ed59ec28d8b7a0543f3
MSA-BD <140373331+MSA-BD@users.noreply.github.com>
2023-08-19 12:09:42 +0600

    exploring all array mrythods

--- iterativeArray.js
+++ iterativeArray.js
@@ -3,5 +3,5 @@ Kichu method main array ke poriborton kore na, main array er refference nijer mo
 Array er primitive type valuer kkhetre value gulu notun array-te copy hoy.
 Mutating array method ke non-mutate e convert korar jonno existing array ke kuno non-muatate method othoba spread-syntex diye access kore nite hoy: arr.slice().copyWithin(1,2,3)
 const value=[...arr].copywithin(1,2,3)
-*/ 
 //These method some, every, find, findIndex, findLast, and findLastIndex do not always invoked the callback function, they stop iteration as soon as they return value is detremined.
+*/ 
