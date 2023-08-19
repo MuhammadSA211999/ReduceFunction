69d572d08c19298844082e274a3f6db63f2f9aee
MSA-BD <140373331+MSA-BD@users.noreply.github.com>
2023-08-19 13:14:27 +0600

    remove multiple item begenning from an array

--- iterativeArray.js
+++ iterativeArray.js
@@ -8,16 +8,34 @@ const value=[...arr].copywithin(1,2,3)
 
 // includes and indexOf method
 //kun ekti item arrayte ache kina ta check korar jonno: includes()/indexOf() method er vitore item er nam argument hisabe pass korte hoy; includes()=>boolean and indexOf()=> int/-1 return kore
-const fruits = ['ban', 'apl', 'jack', 'jack']
+const fruits = ["apple", "banana", "strawberry", "mango", "cherry", "jack"]
 const result = fruits.indexOf('gg')
 // console.log('ban'); -1 // array te item takle index, na takle -1 return kore
-const blResult = fruits.includes('apl')
+const blResult = fruits.includes('apple')
 // console.log(blResult); true //array te item takle true, na takle false return kore
 
-// remove last item from an arrray 
-const lastIndex = fruits.findLastIndex((item) => item === 'jack')
-console.log(lastIndex) // 3 
-const threeIndexItem = fruits[3]
-const newArray = fruits.filter((item) => item !== threeIndexItem)
-console.log(newArray) //['ban', 'apl']
+// remove last item from an arrray
+// const lastIndex = fruits.findLastIndex((item) => item === 'jack')
+// console.log(lastIndex) // 3
+
+// const threeIndexItem = fruits[3]
+// const newArray = fruits.filter((item) => item !== threeIndexItem)
+// console.log(newArray) //['ban', 'apl']
+
+//remove last items from an array by splice() method, **splice() orginal array ke mutate kore.
+// const endsThree = 3 //(minus rigth teke remove kore)
+// const removedArray = fruits.splice(endsThree)
+// console.log(removedArray); //['mango', 'cherry', 'jack']
+// console.log(fruits);//['apple', 'banana', 'strawberry']
+
+//remove firs n number of item from an array by splice(start, end) method
+// const firstThree = 3
+// const start = 0
+// const removeItems = fruits.splice(start, firstThree)
+// console.log(removeItems); //['apple', 'banana', 'strawberry']
+// console.log(fruits); //['mango', 'cherry', 'jack']
+
+
+
+
 
