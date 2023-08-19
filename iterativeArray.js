053125a4fc4478043a9a6b0f1ed155684e59e0bd
MSA-BD <140373331+MSA-BD@users.noreply.github.com>
2023-08-19 12:34:52 +0600

    result

--- iterativeArray.js
+++ iterativeArray.js
@@ -6,10 +6,18 @@ const value=[...arr].copywithin(1,2,3)
 //These method some, every, find, findIndex, findLast, and findLastIndex do not always invoked the callback function, they stop iteration as soon as they return value is detremined.
 */
 
+// includes and indexOf method
 //kun ekti item arrayte ache kina ta check korar jonno: includes()/indexOf() method er vitore item er nam argument hisabe pass korte hoy; includes()=>boolean and indexOf()=> int/-1 return kore
-const fruits = ['ban', 'apl', 'jack']
+const fruits = ['ban', 'apl', 'jack', 'jack']
 const result = fruits.indexOf('gg')
-// console.log('ban'); // array te item takle index, na takle -1 return kore
+// console.log('ban'); -1 // array te item takle index, na takle -1 return kore
 const blResult = fruits.includes('apl')
-console.log(blResult); //array te item takle true, na takle false return kore
+// console.log(blResult); true //array te item takle true, na takle false return kore
+
+// remove last item from an arrray 
+const lastIndex = fruits.findLastIndex((item) => item === 'jack')
+console.log(lastIndex) // 3 
+const threeIndexItem = fruits[3]
+const newArray = fruits.filter((item) => item !== threeIndexItem)
+console.log(newArray) //['ban', 'apl']
 
